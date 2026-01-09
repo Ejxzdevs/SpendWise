2f787fe7a49236f7d7970dd47b52bec955f75b62
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-10 04:35:56 +0800

    feat(auth): implement register API function

--- frontend/services/authServices.ts
+++ frontend/services/authServices.ts
@@ -1,13 +1,33 @@
 import apiClient from "./apiClient";
-import { LoginPayload, LoginResponse } from "@/types/auth";
+import { UserPayload, LoginResponse, RegisterResponse } from "@/types/auth";
 
-export const login = async (payload: LoginPayload): Promise<LoginResponse> => {
+// user login service
+export const login = async (payload: UserPayload): Promise<LoginResponse> => {
   try {
     const response = await apiClient.post<LoginResponse>(
       "/auth/login",
       payload
     );
 
+    return response.data;
+  } catch (err: any) {
+    if (err.response && err.response.data) {
+      throw new Error(err.response.data.message);
+    } else {
+      throw new Error("Network or unknown error");
+    }
+  }
+};
+
+// user registration service
+export const register = async (
+  payload: UserPayload
+): Promise<RegisterResponse> => {
+  try {
+    const response = await apiClient.post<RegisterResponse>(
+      "/auth/register",
+      payload
+    );
     return response.data;
   } catch (err: any) {
     if (err.response && err.response.data) {
