d2326222f716cc2e14bf75814eb3eaf75cfe9d45
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-13 06:53:48 +0800

    feat: add label dashboard for index as default

--- frontend/app/(app)/(tabs)/_layout.tsx
+++ frontend/app/(app)/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function TabsLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="index" options={{ title: "Dashboard" }} />
       <Stack.Screen name="explore" options={{ title: "Explore" }} />
     </Stack>
   );
