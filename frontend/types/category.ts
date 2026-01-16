4c055a2ab6adb502e33d3f49c41c21f04f831b34
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-17 07:01:36 +0800

    feat: add type for Expense categories

--- frontend/types/category.ts
+++ frontend/types/category.ts
@@ -1,16 +1,23 @@
-// Expense category type
-export type expenseCategory =
-  | "Salary"
-  | "Freelance"
-  | "Investment"
-  | "Gift"
-  | "Other";
+import { Ionicons } from "@expo/vector-icons";
 
-// Expense category to Ionicons mapping
-export const expenseCategoryIcons = {
+// here we define the IconName type based on Ionicons glyphMap
+type IconName = keyof typeof Ionicons.glyphMap;
+
+// Income categories
+export const incomeCategoryIcons: Record<string, IconName> = {
   Salary: "cash-outline",
   Freelance: "laptop-outline",
   Investment: "trending-up-outline",
   Gift: "gift-outline",
   Other: "wallet-outline",
-} as const;
+};
+
+// Expense categories
+export const expenseCategoryIcons: Record<string, IconName> = {
+  Food: "fast-food-outline",
+  Transport: "car-outline",
+  Rent: "home-outline",
+  Utilities: "flash-outline",
+  Entertainment: "film-outline",
+  Others: "pricetag-outline",
+};
