dad2cc619f4211ea4ccc87074c4e7442047c5490
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-17 03:36:12 +0800

    feat(controller): add createIncome method

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,20 @@
+import IncomeServices from "../services/incomeServices.js";
+
+class IncomeController {
+  createIncome = async (req, res) => {
+    const { source, amount, description } = req.body;
+    if (!source || !amount)
+      return res
+        .status(400)
+        .json({ success: false, message: "Required fields missing" });
+
+    try {
+      await IncomeServices.createIncome({ source, amount, description });
+      res.status(201).json({ success: true, message: "Income created" });
+    } catch (error) {
+      res.status(500).json({ success: false, message: error.message });
+    }
+  };
+}
+
+export default new IncomeController();
