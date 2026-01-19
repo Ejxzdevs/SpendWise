a456184f1ae481c4197f6989d323892f25192404
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-19 09:11:46 +0800

    refactor(expenses): attach user_id and add Redis caching

--- backend/services/expenseServices.js
+++ backend/services/expenseServices.js
@@ -3,29 +3,34 @@ import ExpensesModel from "../models/expenseModel.js";
 
 class ExpenseServices {
   // CREATE EXPENSE + invalidate cache
-  static async createExpense({ category, amount, description }) {
+  static async createExpense({ userId, category, amount, description }) {
     // Create new expense in DB
     const newExpense = await ExpensesModel.createExpense({
+      userId,
       category,
       amount,
       description,
     });
 
     // Invalidate Redis cache
-    await redisClient.del("expenses:all");
+    await redisClient.del(`expenses:${userId}`);
 
     return newExpense;
   }
 
   // GET ALL EXPENSES
-  static async getAllExpenses() {
+  static async getAllExpenses(userId) {
     // Check Redis cache first
-    const cached = await redisClient.get("expenses:all");
+    const cached = await redisClient.get(`expenses:${userId}`);
     if (cached) return JSON.parse(cached);
 
     // If not in cache, fetch from DB
-    const expenses = await ExpensesModel.getAllExpenses();
-    await redisClient.setEx("expenses:all", 300, JSON.stringify(expenses));
+    const expenses = await ExpensesModel.getAllExpenses(userId);
+    await redisClient.setEx(
+      `expenses:${userId}`,
+      300,
+      JSON.stringify(expenses),
+    );
     return expenses;
   }
 }
