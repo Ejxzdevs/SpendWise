546c2b01f12456eb2df5876a5bd53ef1b3ce6c1b
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-19 09:07:58 +0800

    feat(expenses): attach logged-in user ID to expense creation and retrieval

--- backend/controllers/expensesController.js
+++ backend/controllers/expensesController.js
@@ -2,6 +2,7 @@ import ExpenseServices from "../services/expenseServices.js";
 
 class ExpensesController {
   createExpense = async (req, res) => {
+    const userId = req.user.id;
     const { category, amount, description } = req.body;
     if (!category || !amount)
       return res
@@ -9,7 +10,12 @@ class ExpensesController {
         .json({ success: false, message: "Required fields missing" });
 
     try {
-      await ExpenseServices.createExpense({ category, amount, description });
+      await ExpenseServices.createExpense({
+        userId,
+        category,
+        amount,
+        description,
+      });
       res.status(201).json({ success: true, message: "Expense created" });
     } catch (error) {
       res.status(500).json({ success: false, message: error.message });
@@ -17,8 +23,9 @@ class ExpensesController {
   };
 
   getAllExpenses = async (req, res) => {
+    const userId = req.user.id;
     try {
-      const expenses = await ExpenseServices.getAllExpenses();
+      const expenses = await ExpenseServices.getAllExpenses(userId);
       res.status(200).json({ success: true, data: expenses });
     } catch (err) {
       res.status(500).json({ success: false, message: err.message });
