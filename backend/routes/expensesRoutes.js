ce5fdeb0eba81e12b9b4e3d39dbb924bf1e9a23e
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-19 09:13:02 +0800

    refactor(expenses): add authenticateUser middleware to protect routes

--- backend/routes/expensesRoutes.js
+++ backend/routes/expensesRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import ExpenseController from "../controllers/expensesController.js";
+import { authenticateUser } from "../middleware/authmiddleware.js"; // all lowercase
 
 const router = Router();
 
 // List of expense routes
-router.post("/create", ExpenseController.createExpense);
-router.get("/products", ExpenseController.getAllExpenses);
+router.post("/create", authenticateUser, ExpenseController.createExpense);
+router.get("/products", authenticateUser, ExpenseController.getAllExpenses);
 
 export default router;
