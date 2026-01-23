425ed4194840ccf737e9116520577e98d3b5b261
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-23 08:47:48 +0800

    feat: add authenticator middleware

--- backend/routes/incomeRoutes.js
+++ backend/routes/incomeRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import IncomeController from "../controllers/incomeController.js";
+import { authenticateUser } from "../middleware/authmiddleware.js";
+
 const router = Router();
 
 // List of income routes
-router.post("/create", IncomeController.createIncome);
-router.get("/all", IncomeController.getAllIncomes);
+router.post("/create", authenticateUser, IncomeController.createIncome);
+router.get("/all", authenticateUser, IncomeController.getAllIncomes);
 
 export default router;
