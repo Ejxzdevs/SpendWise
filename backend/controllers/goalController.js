e0fab957b5c93c9444585f9a9924ceddd3b80068
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-28 12:44:21 +0800

    feat: implement goal controller for create and fetch

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,7 +3,8 @@ import GoalServices from "../services/goalServices.js";
 class GoalController {
   createGoal = async (req, res) => {
     const userId = req.user.id;
-    const { goal_name, target_amount, target_date, description } = req.body;
+    const { goal_name, target_amount, target_date, description, icon_name } =
+      req.body;
     if (!goal_name || !target_amount || !target_date) {
       return res.status(400).json({
         success: false,
@@ -19,6 +20,7 @@ class GoalController {
         target_amount,
         target_date,
         description,
+        icon_name,
       });
       res.status(201).json({ success: true, message: "Goal created" });
     } catch (error) {
