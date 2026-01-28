261a646e62cbd1c8ee91bcbe313262ee8094bcde
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-28 12:40:41 +0800

    init

--- frontend/app/(app)/(tabs)/income.tsx
+++ frontend/app/(app)/(tabs)/income.tsx
@@ -14,7 +14,7 @@ import {
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
-import { saveIncome, fetchIncomes } from "@/services/incomeService";
+import { saveIncome, fetchIncomes } from "@/services/incomeServices";
 import { IncomeItem } from "@/types/income";
 import { incomeCategoryIcons } from "@/types/category";
 
