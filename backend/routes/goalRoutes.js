29c42017929531a8b1e51b1e575570b22b17df28
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-29 16:55:32 +0800

    feat: add DELETE /:id route to delete a goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/", authenticateUser, goalController.createGoal);
 router.get("/", authenticateUser, goalController.getAllGoals);
 router.patch("/:id", authenticateUser, goalController.addMoney);
+router.delete("/:id", authenticateUser, goalController.delGoal);
 
 export default router;
