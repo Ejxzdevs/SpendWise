6edba7fa58067ae0dd283dbb6d8fedce0ca27404
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-01-31 13:24:31 +0800

    feat: create method to delete specific expense

--- backend/models/expenseModel.js
+++ backend/models/expenseModel.js
@@ -31,6 +31,20 @@ class ExpensesModel {
       throw new Error("Error fetching expenses");
     }
   }
+
+  // Delete expense by ID
+  static async deleteExpense(expenseId) {
+    const sql = `
+      DELETE FROM expenses WHERE expense_id = $1 RETURNING *;
+    `;
+    try {
+      const result = await _query(sql, [expenseId]);
+      return result.rowCount > 0;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error deleting expense");
+    }
+  }
 }
 
 export default ExpensesModel;
