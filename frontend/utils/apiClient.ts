7e388eb01dc20e24e289f8b4bba73f23bb90fe27
Ejxzdevs <johnerickgofredo163@gmail.com>
2026-02-05 19:33:40 +0800

    init

--- frontend/utils/apiClient.ts
+++ frontend/utils/apiClient.ts
@@ -1,9 +1,11 @@
 // apiClient.ts
 import axios, { AxiosInstance } from "axios";
 
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+
 // Create the Axios instance
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
